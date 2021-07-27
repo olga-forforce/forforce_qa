2197ca6847c83fa58919dac1f8b81fcca28fd929
Ольга Перерва <olga.pererva+qa@forforce.com>
2021-07-27 21:09:21 +0300

    'gulp'

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,11 @@ var sass = require('gulp-sass')(require('sass'));
 sass.compiler = require('node-sass');
 
 gulp.task('scss', function () {
-    return gulp.src('./src//*.scss')
+    return gulp.src('./src/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('sass:watch', function () {
-    gulp.watch('./sass//*.scss', ['sass']);
+gulp.task('scss:watch', function () {
+    gulp.watch('./src/**/*.scss', gulp.series(['scss']));
 });
