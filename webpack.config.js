f138c22e771a37f3854f41ad95e4853c637d5dac
Ольга Перерва <olga.pererva+qa@forforce.com>
2021-08-31 20:21:31 +0300

    'hw26.08'

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: './file.js',
+    entry: {
+       index: [
+           path.resolve(__dirname, "./src/js/file.js"),
+           path.resolve(__dirname, "./src/scss/index.scss"),
+       ]
+    },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'my-first-webpack.bundle.js'
-    }
+        path: path.resolve(__dirname, 'dist/'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+          filename: '../css/[name].css'
+        }),
+]
 };
